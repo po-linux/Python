c138f6b04d2be27c4b2320a2a976ff3a11cdd7e7
Narek <mnk80898@gmail.com>
2020-03-17 13:23:28 +0300

    Added names

---
+++ names.py
@@ -0,0 +1,15 @@
+# На вход подаётся список имён.
+# Найти и вывести самое длинное имя
+# и его длину соответственно.
+
+inp = input()
+names = inp.split(' ')
+
+max_name = names[0]
+for name in names:
+    if len(name) >= len(max_name):
+        max_name = name
+
+print(max_name, len(max_name))
+
+#   print(name, end = '!\n')
